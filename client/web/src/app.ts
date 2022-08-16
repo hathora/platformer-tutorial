d60d8dc36fd8e30fe7c22ad22a8168fed9067e84
Harsh Pandey <hpandey793@gmail.com>
2022-08-16 14:50:27 -0400

    add phaser client

---
+++ client/web/src/app.ts
@@ -0,0 +1,169 @@
+import Phaser from "phaser";
+import { InterpolationBuffer } from "interpolation-buffer";
+
+import { HathoraClient, UpdateArgs } from "../../.hathora/client";
+import { ConnectionFailure } from "../../.hathora/failures";
+import { GameState, Player, UserId, XDirection, YDirection } from "../../../api/types";
+
+export class GameScene extends Phaser.Scene {
+  private stateBuffer: InterpolationBuffer<GameState> | undefined;
+  private players: Map<UserId, Phaser.GameObjects.Sprite> = new Map();
+
+  constructor() {
+    super("game");
+  }
+
+  preload() {
+    this.load.image("background", "background.png");
+    this.load.image("platform_long", "platform_long.png");
+    this.load.image("platform_short", "platform_short.png");
+    this.load.spritesheet("player", "player.png", { frameWidth: 32, frameHeight: 32 });
+  }
+
+  create() {
+    this.add.tileSprite(0, 0, 800, 600, "background").setOrigin(0, 0);
+
+    this.anims.create({
+      key: "idle",
+      frames: this.anims.generateFrameNumbers("player", { start: 0, end: 10 }),
+      frameRate: 15,
+    });
+    this.anims.create({
+      key: "walk",
+      frames: this.anims.generateFrameNumbers("player", { start: 11, end: 22 }),
+    });
+    this.anims.create({
+      key: "jump",
+      frames: [{ key: "player", frame: 23 }],
+    });
+    this.anims.create({
+      key: "fall",
+      frames: [{ key: "player", frame: 24 }],
+    });
+
+    const client = new HathoraClient();
+
+    client.loginAnonymous().then((token) => {
+      client.create(token, {}).then((stateId) => {
+        client
+          .connect(
+            token,
+            stateId,
+            (args) => this._onUpdate(args),
+            (err) => this._onError(err)
+          )
+          .then((connection) => {
+            connection.joinGame({});
+
+            const keys = this.input.keyboard.createCursorKeys();
+            let prevInputs: { horizontal: XDirection; vertical: YDirection } = {
+              horizontal: XDirection.NONE,
+              vertical: YDirection.NONE,
+            };
+            function handleKeyEvt() {
+              const inputs = {
+                horizontal: keys.left.isDown ? XDirection.LEFT : keys.right.isDown ? XDirection.RIGHT : XDirection.NONE,
+                vertical: keys.up.isDown ? YDirection.UP : keys.down.isDown ? YDirection.DOWN : YDirection.NONE,
+              };
+              if (JSON.stringify(inputs) !== JSON.stringify(prevInputs)) {
+                connection.setDirection(inputs);
+                prevInputs = inputs;
+              }
+            }
+
+            this.input.keyboard.on("keydown", handleKeyEvt);
+            this.input.keyboard.on("keyup", handleKeyEvt);
+          });
+      });
+    });
+  }
+
+  update() {
+    if (this.stateBuffer === undefined) {
+      return;
+    }
+
+    const { state } = this.stateBuffer.getInterpolatedState(Date.now());
+
+    state.players.forEach((player) => {
+      if (!this.players.has(player.id)) {
+        this.addPlayer(player);
+      } else {
+        this.updatePlayer(player);
+      }
+    });
+  }
+
+  private addPlayer({ id, x, y }: Player) {
+    const sprite = new Phaser.GameObjects.Sprite(this, x, y, "player").setOrigin(0, 0);
+    this.add.existing(sprite);
+    this.players.set(id, sprite);
+  }
+
+  private updatePlayer({ id, x, y, xDirection, yDirection }: Player) {
+    const sprite = this.players.get(id)!;
+
+    if (xDirection === XDirection.LEFT) {
+      sprite.setFlipX(true);
+      sprite.anims.play("walk", true);
+    } else if (xDirection === XDirection.RIGHT) {
+      sprite.setFlipX(false);
+      sprite.anims.play("walk", true);
+    } else {
+      // do nothing
+    }
+
+    if (yDirection === YDirection.UP) {
+      sprite.anims.play("jump", true);
+    } else if (yDirection === YDirection.DOWN) {
+      sprite.anims.play("fall", true);
+    }
+
+    if (xDirection === XDirection.NONE && yDirection === YDirection.NONE) {
+      sprite.anims.play("idle", true);
+    }
+
+    sprite.x = x;
+    sprite.y = y;
+  }
+
+  private _onUpdate({ state, updatedAt }: UpdateArgs) {
+    if (this.stateBuffer === undefined) {
+      this.stateBuffer = new InterpolationBuffer(state, 50, lerp);
+    } else {
+      this.stateBuffer.enqueue(state, [], updatedAt);
+    }
+    console.log("state update", state);
+  }
+
+  private _onError(error: ConnectionFailure) {
+    console.error("Error occured", error.message);
+  }
+}
+
+function lerp(from: GameState, to: GameState, pctElapsed: number): GameState {
+  return {
+    players: to.players.map((toPlayer) => {
+      const fromPlayer = from.players.find((p) => p.id === toPlayer.id);
+      return fromPlayer !== undefined ? lerpPlayer(fromPlayer, toPlayer, pctElapsed) : toPlayer;
+    }),
+  };
+}
+
+function lerpPlayer(from: Player, to: Player, pctElapsed: number): Player {
+  return {
+    id: to.id,
+    xDirection: to.xDirection,
+    yDirection: to.yDirection,
+    x: from.x + (to.x - from.x) * pctElapsed,
+    y: from.y + (to.y - from.y) * pctElapsed,
+  };
+}
+
+new Phaser.Game({
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: [GameScene],
+  parent: "phaser-container",
+});
--- server/impl.ts
+++ server/impl.ts
@@ -1,9 +1,11 @@
+import { ArcadePhysics } from "arcade-physics";
+import { Body } from "arcade-physics/lib/physics/arcade/Body";
+
 import { Methods, Context } from "./.hathora/methods";
 import { Response } from "../api/base";
 import {
   XDirection,
   YDirection,
-  Player,
   GameState,
   UserId,
   IInitializeRequest,
@@ -11,11 +13,27 @@ import {
   ISetDirectionRequest,
 } from "../api/types";
 
-type InternalState = GameState;
+type InternalPlayer = {
+  id: UserId;
+  body: Body;
+  xDirection: XDirection;
+  yDirection: YDirection;
+};
+type InternalState = {
+  physics: ArcadePhysics;
+  players: InternalPlayer[];
+};
 
 export class Impl implements Methods<InternalState> {
   initialize(ctx: Context, request: IInitializeRequest): InternalState {
     return {
+      physics: new ArcadePhysics({
+        sys: {
+          game: { config: {} },
+          settings: { physics: { gravity: { y: 200 } } },
+          scale: { width: 800, height: 600 },
+        },
+      }),
       players: [],
     };
   }
@@ -23,10 +41,14 @@ export class Impl implements Methods<InternalState> {
     if (state.players.some((player) => player.id === userId)) {
       return Response.error("Already joined");
     }
+    const playerBody = state.physics.add.body(0, 0, 32, 32);
+    playerBody.pushable = false;
+    // @ts-ignore
+    playerBody.setCollideWorldBounds(true);
+    state.players.forEach((player) => state.physics.add.collider(playerBody, player.body));
     state.players.push({
       id: userId,
-      x: 0,
-      y: 0,
+      body: playerBody,
       xDirection: XDirection.NONE,
       yDirection: YDirection.NONE,
     });
@@ -42,6 +64,38 @@ export class Impl implements Methods<InternalState> {
     return Response.ok();
   }
   getUserState(state: InternalState, userId: UserId): GameState {
-    return state;
+    return {
+      players: state.players.map((player) => {
+        const xVelocity = player.body.velocity.x;
+        const yVelocity = player.body.velocity.y;
+        return {
+          id: player.id,
+          x: player.body.x,
+          y: player.body.y,
+          xDirection: xVelocity < 0 ? XDirection.LEFT : xVelocity > 0 ? XDirection.RIGHT : XDirection.NONE,
+          yDirection: yVelocity < 0 ? YDirection.UP : yVelocity > 0 ? YDirection.DOWN : YDirection.NONE,
+        };
+      }),
+    };
+  }
+  onTick(state: InternalState, ctx: Context, timeDelta: number): void {
+    state.players.forEach((player) => {
+      if (player.xDirection === XDirection.LEFT && !player.body.blocked.left) {
+        player.body.setVelocityX(-200);
+      } else if (player.xDirection === XDirection.RIGHT && !player.body.blocked.right) {
+        player.body.setVelocityX(200);
+      } else if (player.xDirection === XDirection.NONE) {
+        player.body.setVelocityX(0);
+      }
+      if (player.yDirection === YDirection.UP && player.body.blocked.down) {
+        player.body.setVelocityY(-200);
+      } else if (player.yDirection === YDirection.DOWN && !player.body.blocked.down) {
+        player.body.setVelocityY(150);
+      }
+    });
+
+    if (!state.players.every(({ body }) => body.velocity.x === 0 && body.velocity.y === 0 && body.blocked.down)) {
+      state.physics.world.update(ctx.time, timeDelta * 1000);
+    }
   }
 }
